1218dbadb7b16a52dc57b1fde324d43f5b6e0b03
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-02-27 10:33:35 +0700

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:url_strategy/url_strategy.dart';
 
+import 'ui/screens/home_page.dart';
+
 void main() {
   setPathUrlStrategy();
   runApp(MyApp());
@@ -9,6 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomePage.id: (context) => HomePage(),
+      },
+    );
   }
 }
---
+++ lib/ui/screens/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static const String id = '/';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Ini Home Page"),
+      ),
+    );
+  }
+}
