ffca0f23919fa3d9e22b6d9de3f550c05be0030c
TjandraD <t.darmo.t@gmail.com>
2021-03-06 08:42:04 +0700

    feat: add navigation in ontap program card function

--- lib/widgets/program_card.dart
+++ lib/widgets/program_card.dart
@@ -1,4 +1,5 @@
 import 'package:crowdfunding_web/common/utils.dart';
+import 'package:crowdfunding_web/pages/donation_page.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -142,20 +143,26 @@ class _ProgramCardState extends State<ProgramCard> {
                           style: subtitleTextStyle,
                         ),
                       ),
-                      Container(
-                        margin: EdgeInsets.only(top: defaultPadding * 2),
-                        width: double.infinity,
-                        height: 30,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: buttonColor,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text(
-                          "DONASI SEKARANG",
-                          style: Theme.of(context).textTheme.bodyText2.copyWith(
-                                color: Colors.white,
-                              ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, DonationPage.id);
+                        },
+                        child: Container(
+                          margin: EdgeInsets.only(top: defaultPadding * 2),
+                          width: double.infinity,
+                          height: 30,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: buttonColor,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text(
+                            "DONASI SEKARANG",
+                            style:
+                                Theme.of(context).textTheme.bodyText2.copyWith(
+                                      color: Colors.white,
+                                    ),
+                          ),
                         ),
                       ),
                     ],
