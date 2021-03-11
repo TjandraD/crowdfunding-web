9779021ec9065363ac9297c742914f041393bdda
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-03-11 09:53:37 +0700

    connect to firestore

---
+++ lib/services/firestore_services.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class FirestoreServices {
+  static FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  static CollectionReference _program = _firestore.collection('donasi');
+
+  // static Future<String> addDonationProgram({
+  //   @required String programDetail,
+  //   @required String programName,
+  //   @required String programImagePath,
+  //   @required String totalFunds,
+  //   @required String fundRaised,
+  // }) async {
+  //   try {
+  //     await _program.add({
+  //       'programName': programName,
+  //       'programDetail': programDetail,
+  //       'programImagePath': programImagePath,
+  //       'totalFunds': totalFunds,
+  //       'fundRaised': fundRaised,
+  //     });
+  //     return 'berhasil';
+  //   } catch (error) {
+  //     return error.message;
+  //   }
+  // }
+
+  static Stream<QuerySnapshot> getDonationProgram() {
+    return _program.snapshots();
+  }
+}
