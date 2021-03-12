1dc936fe49c0ae383790e9fad93c0f311f1d8f20
TjandraD <t.darmo.t@gmail.com>
2021-03-12 20:14:55 +0700

    ui: add responsive design in report detail page

--- lib/pages/report_detail_page.dart
+++ lib/pages/report_detail_page.dart
@@ -1,127 +1,249 @@
 import 'package:crowdfunding_web/common/utils.dart';
 import 'package:crowdfunding_web/widgets/customAppBar/customAppBarDesktop.dart';
+import 'package:crowdfunding_web/widgets/customAppBar/customAppBarMobile.dart';
+import 'package:crowdfunding_web/widgets/customBottomNavBar.dart';
 import 'package:crowdfunding_web/widgets/footer.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class ReportDetail extends StatelessWidget {
   static const String id = '/report/detail';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFF3F3F3),
-      appBar: CustomAppBarDesktopTablet(),
-      body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        return Scaffold(
+          backgroundColor: Color(0xFFF3F3F3),
+          appBar: (sizingInformation.isMobile)
+              ? CustomAppBarMobile()
+              : CustomAppBarDesktopTablet(),
+          body: Scrollbar(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: defaultPadding * 4),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: defaultPadding * 8,
-                          ),
-                          Image.asset('assets/images/Mosque.png'),
-                          SizedBox(
-                            height: defaultPadding,
-                          ),
-                          Text(
-                            "DUKUNG PEMBANGUNAN MASJID DI SMK WIKRAMA BOGOR",
-                            style:
-                                Theme.of(context).textTheme.headline6.copyWith(
-                                      color: buttonColor,
-                                      fontWeight: FontWeight.bold,
+                  (sizingInformation.isMobile)
+                      ? Container(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: defaultPadding * 4),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: defaultPadding * 8,
                                     ),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            "Masjid yang didirikan akan digunakan oleh para siswa/i di SMK Wikrama Bogor",
-                            style: Theme.of(context).textTheme.bodyText2,
-                            textAlign: TextAlign.left,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(defaultPadding * 4),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            'Riwayat Penggunaan Dana',
-                            style: Theme.of(context).textTheme.headline6,
-                            textAlign: TextAlign.left,
-                          ),
-                          TimelineTile(
-                            endChild: ReportCard(),
-                            indicatorStyle: IndicatorStyle(
-                              height: 16.0,
-                              width: 16.0,
-                              padding: EdgeInsets.symmetric(
-                                vertical: defaultPadding * 0.25,
+                                    Image.asset('assets/images/Mosque.png'),
+                                    SizedBox(
+                                      height: defaultPadding,
+                                    ),
+                                    Text(
+                                      "DUKUNG PEMBANGUNAN MASJID DI SMK WIKRAMA BOGOR",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6
+                                          .copyWith(
+                                            color: buttonColor,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    Text(
+                                      "Masjid yang didirikan akan digunakan oleh para siswa/i di SMK Wikrama Bogor",
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                  ],
+                                ),
                               ),
-                              indicator: Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                                  border: Border.all(
-                                    color: Color(0xFFe08283),
-                                    width: 2.0,
-                                  ),
+                              Container(
+                                padding: EdgeInsets.all(defaultPadding * 4),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      'Riwayat Penggunaan Dana',
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    TimelineTile(
+                                      endChild: ReportCard(),
+                                      indicatorStyle: IndicatorStyle(
+                                        height: 16.0,
+                                        width: 16.0,
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: defaultPadding * 0.25,
+                                        ),
+                                        indicator: Container(
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                            border: Border.all(
+                                              color: Color(0xFFe08283),
+                                              width: 2.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      afterLineStyle: LineStyle(
+                                        color: Colors.black54,
+                                        thickness: 2.0,
+                                      ),
+                                      isFirst: true,
+                                    ),
+                                    TimelineTile(
+                                      endChild: ReportCard(),
+                                      indicatorStyle: IndicatorStyle(
+                                        height: 16.0,
+                                        width: 16.0,
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: defaultPadding * 0.25,
+                                        ),
+                                        indicator: Container(
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                            border: Border.all(
+                                              color: Color(0xFFe08283),
+                                              width: 2.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      beforeLineStyle: LineStyle(
+                                        color: Colors.black54,
+                                        thickness: 2.0,
+                                      ),
+                                      isLast: true,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding:
+                                    EdgeInsets.only(left: defaultPadding * 4),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: defaultPadding * 8,
+                                    ),
+                                    Image.asset('assets/images/Mosque.png'),
+                                    SizedBox(
+                                      height: defaultPadding,
+                                    ),
+                                    Text(
+                                      "DUKUNG PEMBANGUNAN MASJID DI SMK WIKRAMA BOGOR",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6
+                                          .copyWith(
+                                            color: buttonColor,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    Text(
+                                      "Masjid yang didirikan akan digunakan oleh para siswa/i di SMK Wikrama Bogor",
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-                            afterLineStyle: LineStyle(
-                              color: Colors.black54,
-                              thickness: 2.0,
-                            ),
-                            isFirst: true,
-                          ),
-                          TimelineTile(
-                            endChild: ReportCard(),
-                            indicatorStyle: IndicatorStyle(
-                              height: 16.0,
-                              width: 16.0,
-                              padding: EdgeInsets.symmetric(
-                                vertical: defaultPadding * 0.25,
-                              ),
-                              indicator: Container(
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                                  border: Border.all(
-                                    color: Color(0xFFe08283),
-                                    width: 2.0,
-                                  ),
+                            Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.all(defaultPadding * 4),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Text(
+                                      'Riwayat Penggunaan Dana',
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                      textAlign: TextAlign.left,
+                                    ),
+                                    TimelineTile(
+                                      endChild: ReportCard(),
+                                      indicatorStyle: IndicatorStyle(
+                                        height: 16.0,
+                                        width: 16.0,
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: defaultPadding * 0.25,
+                                        ),
+                                        indicator: Container(
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                            border: Border.all(
+                                              color: Color(0xFFe08283),
+                                              width: 2.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      afterLineStyle: LineStyle(
+                                        color: Colors.black54,
+                                        thickness: 2.0,
+                                      ),
+                                      isFirst: true,
+                                    ),
+                                    TimelineTile(
+                                      endChild: ReportCard(),
+                                      indicatorStyle: IndicatorStyle(
+                                        height: 16.0,
+                                        width: 16.0,
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: defaultPadding * 0.25,
+                                        ),
+                                        indicator: Container(
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.white,
+                                            border: Border.all(
+                                              color: Color(0xFFe08283),
+                                              width: 2.0,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      beforeLineStyle: LineStyle(
+                                        color: Colors.black54,
+                                        thickness: 2.0,
+                                      ),
+                                      isLast: true,
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-                            beforeLineStyle: LineStyle(
-                              color: Colors.black54,
-                              thickness: 2.0,
-                            ),
-                            isLast: true,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                          ],
+                        ),
+                  Footer(),
                 ],
               ),
-              Footer(),
-            ],
+            ),
           ),
-        ),
-      ),
+          bottomNavigationBar:
+              (sizingInformation.isMobile) ? CustomBottomNavBar() : null,
+        );
+      },
     );
   }
 }
